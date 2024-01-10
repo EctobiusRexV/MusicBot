0a015da5431d1dbc2e27d89a64ebe72fb07ccb0a
SacFeve <antmorin374@outlook.com>
2024-01-09 21:54:04 -0500

    First command Request.py pour requetes d'API

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ import settings
 
 def run():
     intents = discord.Intents.default()
+    intents.message_content = True
     bot = commands.Bot(command_prefix = '!', intents = intents)
-    bot.run(settings.discord_token)
+
+    @bot.command(name = 'play')
+    async def play(ctx, url):
+        await ctx.send(f'Playing {url}...')
 
     @bot.event
     async def on_ready():
         print(f'{bot.user} has connected to Discord!')
+
+    bot.run(settings.discord_token)
 
 if __name__ == '__main__':
     run()
